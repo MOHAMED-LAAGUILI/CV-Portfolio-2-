680998886d6da3d32696da2190f88a2155ca66c5
MOHAMED LAAGUILI | Junior Dev <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2024-12-03 10:40:45 +0100

    Add files via upload

--- assets/js/ContactFormMessage.js
+++ assets/js/ContactFormMessage.js
@@ -1,5 +1,18 @@
 var form = document.getElementById("my-form");
-    
+
+const successMsg = `
+            <div class="alert alert-success alert-dismissible d-flex align-items-center fade show" id="my-form-status">
+            <i class="bi bi-send-check"></i>
+          <strong class="mx-2">Success!</strong> Thank you, your message has been Delivered
+          <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>`;
+
+const errMsg = `<div class="alert alert-danger alert-dismissible d-flex align-items-center fade show">
+            <i class="bi bi-x-circle-fill"></i>
+        <strong class="mx-2">Error!</strong> Oops! Message failed to deliver due to unstable connection
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+      </div>`;
+
 async function handleSubmit(event) {
   event.preventDefault();
   var status = document.getElementById("my-form-status");
@@ -8,29 +21,28 @@ async function handleSubmit(event) {
     method: form.method,
     body: data,
     headers: {
-        'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      status.innerHTML = `
-      <div class="alert alert-success alert-dismissible d-flex align-items-center fade show" id="my-form-status">
-      <i class="bi-check-circle-fill"></i>
-    <strong class="mx-2">Success!</strong> Merci, votre message a ete délivrée
-    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-  </div>`;
-  
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-          status.innerHTML = "Oops! Il y a eu un problème lors de la soumission de votre formulaire"
-        }
-      })
-    }
-  }).catch(error => {
-    status.innerHTML = "Oops! Il y a eu un problème lors de la soumission de votre formulaire"
-  });
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = successMsg;
+
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = errMsg;
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = errMsg;
+    });
 }
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,13 +3,13 @@
   const navItems = [
     { href: "#hero", icon: "./assets/img/favicon.ico", label: "Home", isImage: true },
     { href: "#about", icon: "bx bx-user", label: "About Me", isImage: false },
-    { href: "#achivements", icon: "bx bx-file-blank", label: "Achivement", isImage: false },
-    { href: "#skills", icon: "bx bx-file-blank", label: "Skills", isImage: false },
-    { href: "#resume", icon: "bx bx-file-blank", label: "CV - My Resume", isImage: false },
-    { href: "#services", icon: "bx bx-crosshair", label: "My Services", isImage: false },
-    { href: "#portfolio", icon: "bx bx-crosshair", label: "Portfolios", isImage: false },
-    { href: "#testimonials", icon: "bx bx-comment", label: "Other Opinions", isImage: false },
-    { href: "#contact", icon: "bx bx-envelope", label: "Contact Me", isImage: false },
+    { href: "#achievements", icon: "bi bi-trophy", label: "Achievements", isImage: false },
+    { href: "#skills", icon: "bi bi-stack", label: "Skills", isImage: false },
+    { href: "#resume", icon: "bi bi-file-earmark-arrow-down", label: "CV - Resume", isImage: false },
+    { href: "#services", icon: "bx bx-crosshair", label: "Services", isImage: false },
+    { href: "#portfolio", icon: "bi bi-window-stack", label: "Portfolios", isImage: false },
+    { href: "#testimonials", icon: "bx bx-comment", label: "Others Opinions", isImage: false },
+    { href: "#contact", icon: "bi bi-send", label: "Contact Me", isImage: false },
   ];
 
   // Get the navigation menu element
@@ -20,7 +20,7 @@
     const li = document.createElement("li");
     li.innerHTML = `
       <a href="${item.href}" class="nav-link scrollto">
-        ${item.isImage ? `<img src="${item.icon}" class="rounded-pill floating-img" width="50px" alt="${item.label}" />` : `<i class="${item.icon}"></i>`}
+        ${item.isImage ? `<img src="${item.icon}" class="rounded-pill floating-img" width="48px" alt="${item.label}" />` : `<i class="${item.icon}"></i>`}
         <span>${item.label}</span>
       </a>
     `;
