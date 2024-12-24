292f167aee80a4d87df349b71ffb8856e6c9ff43
MOHAMED-LAAGUILI-vnb <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2024-12-24 16:40:19 +0100

    add content to be nosniff

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -6,39 +6,38 @@ self.addEventListener("install", function (event) {
     caches.open(staticCacheName).then(function (cache) {
       const resourcesToCache = [
         './', // Cache the root (index.html) or other pages you want
-  './assets/img/favicon.png', // Example: your favicon
-  './assets/css/style.css', // Example: your main CSS file
-  './assets/js/main.js', // Example: your main JS file
-  
-  // External resources
-  'https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong', // Font
-  'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i', // Fonts
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css', // Font Awesome
-  'https://unpkg.com/aos@2.3.1/dist/aos.css', // AOS CSS
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', // Bootstrap CSS
-  'https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.2.4/swiper-bundle.min.css', // Swiper CSS
-  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css', // Animate CSS
-  'https://cdn.jsdelivr.net/npm/sweetalert2@11.1.6/dist/sweetalert2.min.css', // SweetAlert CSS
-  'https://cdn.jsdelivr.net/npm/ol@v6.15.0/ol.css', // OpenLayers CSS
-  
-  // External JS resources
-  'https://code.jquery.com/jquery-3.6.0.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.2.4/swiper-bundle.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.8/typed.min.js',
-  'https://unpkg.com/aos@2.3.1/dist/aos.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
-  'https://cdn.jsdelivr.net/npm/glightbox@3.2.0/dist/js/glightbox.min.js',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@11', // SweetAlert2 JS
-  'https://cdnjs.cloudflare.com/ajax/libs/i18next/21.6.0/i18next.min.js', // i18next JS
-  './assets/js/main.js', // Your local JS files
-  './assets/js/nav.js', // Local JS files
+        './assets/img/favicon.png', // Example: your favicon
+        './assets/css/style.css', // Example: your main CSS file
+        './assets/js/main.js', // Example: your main JS file
+
+        // External resources
+        'https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong', // Font
+        'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i', // Fonts
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css', // Font Awesome
+        'https://unpkg.com/aos@2.3.1/dist/aos.css', // AOS CSS
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', // Bootstrap CSS
+        'https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.2.4/swiper-bundle.min.css', // Swiper CSS
+        'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css', // Animate CSS
+        'https://cdn.jsdelivr.net/npm/sweetalert2@11.1.6/dist/sweetalert2.min.css', // SweetAlert CSS
+        'https://cdn.jsdelivr.net/npm/ol@v6.15.0/ol.css', // OpenLayers CSS
+
+        // External JS resources
+        'https://code.jquery.com/jquery-3.6.0.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.2.4/swiper-bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.8/typed.min.js',
+        'https://unpkg.com/aos@2.3.1/dist/aos.js',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+        'https://cdn.jsdelivr.net/npm/glightbox@3.2.0/dist/js/glightbox.min.js',
+        'https://cdn.jsdelivr.net/npm/sweetalert2@11', // SweetAlert2 JS
+        'https://cdnjs.cloudflare.com/ajax/libs/i18next/21.6.0/i18next.min.js', // i18next JS
+        './assets/js/main.js', // Your local JS files
+        './assets/js/nav.js', // Local JS files
         "/index.html", // Ensure this file exists in the correct folder
         "/404.html",    // Ensure this file exists as a fallback page
       ];
 
-      // Log the resources being cached
       console.log("Caching resources:", resourcesToCache);
 
       return cache.addAll(resourcesToCache).catch(function (error) {
@@ -61,10 +60,18 @@ self.addEventListener("fetch", function (event) {
 
       console.log('Cache miss:', event.request.url);
       return fetch(event.request).then(function (networkResponse) {
+        // Add the X-Content-Type-Options header
+        const newHeaders = new Headers(networkResponse.headers);
+        newHeaders.set('X-Content-Type-Options', 'nosniff');
+
         // Cache the new content for future requests
         return caches.open(staticCacheName).then(function (cache) {
           cache.put(event.request, networkResponse.clone());
-          return networkResponse;
+          return new Response(networkResponse.body, {
+            status: networkResponse.status,
+            statusText: networkResponse.statusText,
+            headers: newHeaders
+          });
         });
       }).catch(function () {
         // Provide fallback for offline scenarios
